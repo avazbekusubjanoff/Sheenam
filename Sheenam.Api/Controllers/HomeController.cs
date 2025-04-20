cf8a2a52f1da6f1fad07c1e407bf12b3364db0b8
avazbekusubjanoff <avazbekusubjanoff@gmail.com>
2025-04-20 10:55:45 +0500

    INFRA:Api Project

---
+++ Sheenam.Api/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿//=================================================
+//Copyright (c) Coalition of Good-Hearted Engineers
+//Free To Use Comfort and Peace
+//=================================================
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Get() =>
+            Ok("hello mario");
+    }
+}
